8e89b5dca222f174022a8c56ce6d955d8641f5cc
MarcosPego <marcosppego@gmail.com>
2018-10-21 20:24:13 +0100

    Half way done

--- Projeto 1/project.py
+++ Projeto 1/project.py
@@ -39,7 +39,7 @@ def pos_c (pos):
 #TAI move
 #Lista [p_initial, p_final]
 
-def make_move (i,f):
+def make_move (i, f):
     return[i,f]
 
 def move_initial (move):
@@ -61,9 +61,7 @@ def board_moves(board):
             pos = make_pos(list, column)
             moves.extend(check_adjacent_moves(board, pos, len(board), len(board[0])))
 
-
-    filtered_move = [x for x in moves if x != []]
-    return filtered_move
+    return [x for x in moves if x != []]
 
 
 
@@ -74,41 +72,40 @@ def check_adjacent_moves(board, pos, line_size, column_size):
     move.append(right_move(board, pos, line_size, column_size))
     move.append(upper_move(board, pos, line_size, column_size))
     move.append(lower_move(board, pos, line_size, column_size))
-    filtered_move = [x for x in move if x != []]
-    return filtered_move
+    return [x for x in move if x != []]
 
 
 def left_move(board, pos, line_size, column_size):
-    if pos_c(pos) -2 >= 0:
-        possible_pos = make_pos(pos_l(pos) , pos_c(pos)-2)
-        adjacent_pos = make_pos(pos_l(pos) , pos_c(pos)-1)
+    if pos_c(pos) - 2 >= 0:
+        possible_pos = make_pos(pos_l(pos) , pos_c(pos) - 2)
+        adjacent_pos = make_pos(pos_l(pos) , pos_c(pos) - 1)
         if is_possible_move(board, pos, possible_pos) and is_adjacent_marble(board, pos, adjacent_pos):
             move = make_move(pos, possible_pos)
             return move
     return []
 
 def right_move(board, pos, line_size, column_size):
-    if pos_c(pos) +2 < column_size:
-        possible_pos = make_pos(pos_l(pos) , pos_c(pos)+2)
-        adjacent_pos = make_pos(pos_l(pos) , pos_c(pos)+1)
+    if pos_c(pos) + 2 < column_size:
+        possible_pos = make_pos(pos_l(pos) , pos_c(pos) + 2)
+        adjacent_pos = make_pos(pos_l(pos) , pos_c(pos) + 1)
         if is_possible_move(board, pos, possible_pos) and is_adjacent_marble(board, pos, adjacent_pos):
             move = make_move(pos, possible_pos)
             return move
     return []
 
 def upper_move(board, pos, line_size, column_size):
-    if pos_l(pos) -2 >= 0:
-        possible_pos = make_pos(pos_l(pos)-2, pos_c(pos))
-        adjacent_pos = make_pos(pos_l(pos)-1, pos_c(pos))
+    if pos_l(pos) - 2 >= 0:
+        possible_pos = make_pos(pos_l(pos) - 2, pos_c(pos))
+        adjacent_pos = make_pos(pos_l(pos) - 1, pos_c(pos))
         if is_possible_move(board, pos, possible_pos) and is_adjacent_marble(board, pos, adjacent_pos):
             move = make_move(pos, possible_pos)
             return move
     return []
 
 def lower_move(board, pos, line_size, column_size):
-    if pos_l(pos) +2 < line_size:
-        possible_pos = make_pos(pos_l(pos)+2 , pos_c(pos))
-        adjacent_pos = make_pos(pos_l(pos)+1 , pos_c(pos))
+    if pos_l(pos) + 2 < line_size:
+        possible_pos = make_pos(pos_l(pos) + 2 , pos_c(pos))
+        adjacent_pos = make_pos(pos_l(pos) + 1 , pos_c(pos))
         if is_possible_move(board, pos, possible_pos) and is_adjacent_marble(board, pos, adjacent_pos):
             move = make_move(pos, possible_pos)
             return move
@@ -117,20 +114,14 @@ def lower_move(board, pos, line_size, column_size):
 
 def is_adjacent_marble(board, pos, adjacent_pos):
     if is_peg(board[pos_l(pos)][pos_c(pos)]):
-        if is_peg(board[pos_l(adjacent_pos)][pos_c(adjacent_pos)]):
-            return True
-        else:
-            return False
+        return is_peg(board[pos_l(adjacent_pos)][pos_c(adjacent_pos)])
     else:
         return False
 
 
 def is_possible_move(board, pos, possible_pos):
     if is_peg(board[pos_l(pos)][pos_c(pos)]):
-        if is_empty(board[pos_l(possible_pos)][pos_c(possible_pos)]):
-            return True
-        else:
-            return False
+        return is_empty(board[pos_l(possible_pos)][pos_c(possible_pos)])
     else:
         return False
 
@@ -140,12 +131,10 @@ def board_perform_move(board, move):
     new_board = board.copy()
     new_board = remove_marble(new_board, move[0])
 
-
     middle_position = check_middle_move_position(move)
     new_board = remove_marble(new_board, middle_position)
 
     new_board = add_marble(new_board, move[1])
-
     return new_board
 
 
@@ -177,24 +166,62 @@ def add_marble(board, pos):
     new_board[pos_l(pos)][pos_c(pos)] = c_peg()
     return new_board
 
+
+def check_if_is_solution(board):
+    marble_count = 0;
+
+    for list in range(0,len(board)):
+        for column in range(0, len(board[list])):
+            if is_peg(board[list][column]):
+                marble_count += 1
+
+    return marble_count == 1
+
+
 #sol_state
 
 class sol_state:
     def __init__(self, board):
         self.board = board
+        #self.action = action
 
-    #def __lt (self, other):
-    #    var = 'filler'
+    def __repr__(self):
+        return str(self.board)
 
-class solitare(Problem):
+    def __str__(self):
+        return str(self.board)
+
+    def __eq__(self, other):
+        return self.board== other.board
+
+    def __lt__ (self, other):
+        return self.board < other.board
+
+
+class solitaire(Problem):
     def __init__(self, board):
-        self.p_initial = sol_state(board)
+        self.initial = sol_state(board)
+
+    def actions(self, state):
+        return board_moves(state.board)
+
+    def result(self, state, action):
+        return sol_state(board_perform_move(state.board, action))
+
+    def goal_test(self, state):
+        return check_if_is_solution(state.board)
+
+    '''
+
+    def path_cost(self, c, state1, action, state2):
+
+    def h(self, node):
+    '''
 
 
 
-#b1 = [['O','_','_','O','_'], ['O','_','O','_','O'], ['_','O','_','O','_'],['O','_','O','_','_'], ['_','O','_','_','_']]
-#print(board_perform_move(b1, [(0, 2), (0, 0)]))
+print( depth_first_tree_search(solitaire([["O","O","O","X"],["O","O","O","O"],["O","_","O","O"],["O","O","O","O"]])))
 
-#print(b1)
-#    [["_","O","O","O","_"], ["O","_","O","_","O"], ["_","O","_","O","_"],["O","_","O","_","_"], ["_","O","_","_","_"]]
+#print(solitaire([["X","O","_","O","X"],["O","_","_","_","O"],["_","_","_","_","O"],["O","O","_","_","O"],["X","O","O","O","X"]]).goal_test(sol_state([["X","O","_","O","X"],["O","_","_","_","O"],["_","_","_","_","O"],["O","O","_","_","O"],["X","O","O","O","X"]])))
+
 #    /mnt/c/Users/Utilizador/Documents/GitHub/IA
